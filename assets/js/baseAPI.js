ad676783189fb456191a1c0800878cfd7b5d0370
laojiu <2015048922@qq.com>
2020-09-18 22:40:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,27 @@
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
     //在发起真正的Ajax请求时，统一拼接请求的根路径
     options.url = "http://ajax.frontend.itheima.net" + options.url;
     // console.log(options.url);
-      
+
+    //token身份
+    if (options.url.indexOf("/my/")) {
+        // console.log(10);
+
+        options.header = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+
+    //进行身份验证，没有token身份不能访问后台页面
+    options.complete = function (res) {
+        console.log(res);
+        
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            // console.log(1);
+            localStorage.removeItem("token")
+            location.href = "./login.html"
+        }
+    }
+
 
 })
---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+$(function () {
+    //获取登录用户信息
+    getUserInfo();
+    //退出点击事件
+    $("#exit").on("click", function () {
+        // console.log(1);
+        localStorage.removeItem("token");
+        location.href = "./login.html"
+    }) 
+
+})
+
+
+//获取登录用户的基本信息
+function getUserInfo () {
+    $.ajax({
+        url:"/my/userinfo",
+        method:"GET",
+        // headers:{
+        //     Authorization:localStorage.getItem("token")||""
+        // },
+        success: function (res) {
+            if(res.status == 1) {
+               return layui.layer.msg("登录失败！")
+            }
+            //渲染头像
+            randerAvatar(res.data)
+        },
+
+        //进行身份验证，没有token身份不能访问后台页面
+        // complete: function (res) {
+        //     if(res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！")
+        //     {
+        //         location.href = "./login.html"
+        //     }
+            
+        // }
+    })
+}
+
+
+//渲染头像函数
+function randerAvatar(user) {
+    // console.log(2);
+    //渲染用户名
+    var name = user.nickname || user.username;
+    $("#welcome").html(`<span id="welcome">欢迎&nbsp&nbsp${name}</span>`);
+    //渲染头像
+    if(user.user_pic) {
+        $(".layui-nav-img").attr("src",user.user_pic).show();
+        $(".text-avatar").hide()
+        
+
+    } else {
+        var avatar = name[0].toUpperCase();
+        $(".text-avatar").text(avatar).show();
+        $(".layui-nav-img").hide();
+    }
+}
